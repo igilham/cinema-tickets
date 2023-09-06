a2fdcff7dffc431505ebf5099adf807758bf2415
Ian Gilham <igilham@users.noreply.github.com>
2023-09-06 10:53:14 +0100

    test: check account IDs

--- cinema-tickets-javascript/src/pairtest/TicketService.test.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.test.js
@@ -1,3 +1,15 @@
+import TicketService from "./TicketService.js";
+import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
+
 describe("TicketService", () => {
-  test("should throw an InvalidPurchaseException if the purchase is invalid", () => {});
+  test.each([-235123563, -1, 0, "hello"])(
+    "should reject purchse with invalid account ID %s",
+    (id) => {
+      const ticketService = new TicketService();
+      // @ts-ignore - testing invalid input types
+      expect(() => ticketService.purchaseTickets(id)).toThrow(
+        InvalidPurchaseException
+      );
+    }
+  );
 });
