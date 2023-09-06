1c1892746870326ba66a08b01b0f57e8ab865c35
Ian Gilham <igilham@users.noreply.github.com>
2023-09-06 09:27:47 +0100

    build: setup dependencies, tools
    - update jest
    - remove mocha
    - add tsc
    - add types to existing code with jsDoc

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,11 +1,17 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from "./lib/TicketTypeRequest.js";
+import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
 
+  /**
+   * Purchase tickets
+   * @param {string} accountId
+   * @param  {...TicketTypeRequest} ticketTypeRequests
+   * @throws {InvalidPurchaseException} if the purchase is invalid
+   */
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
   }
--- cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
+++ cinema-tickets-javascript/src/pairtest/lib/TicketTypeRequest.js
@@ -1,32 +1,51 @@
 /**
- * Immutable Object.
+ * @typedef {'ADULT' | 'CHILD' | 'INFANT'} TicketType
  */
 
+/**
+ * Immutable Object.
+ */
 export default class TicketTypeRequest {
+  /**@type {!TicketType} */
   #type;
 
+  /**@type {!number} */
   #noOfTickets;
 
+  /**
+   * @param {!TicketType} type the type of the tickets in the request
+   * @param {!number} noOfTickets the number of tickets in the request
+   */
   constructor(type, noOfTickets) {
     if (!this.#Type.includes(type)) {
-      throw new TypeError(`type must be ${this.#Type.slice(0, -1).join(', ')}, or ${this.#Type.slice(-1)}`);
+      throw new TypeError(
+        `type must be ${this.#Type
+          .slice(0, -1)
+          .join(", ")}, or ${this.#Type.slice(-1)}`
+      );
     }
 
     if (!Number.isInteger(noOfTickets)) {
-      throw new TypeError('noOfTickets must be an integer');
+      throw new TypeError("noOfTickets must be an integer");
     }
 
     this.#type = type;
     this.#noOfTickets = noOfTickets;
   }
 
+  /**
+   * @returns {!number} the number of tickets in the request
+   */
   getNoOfTickets() {
     return this.#noOfTickets;
   }
 
+  /**
+   * @returns {!TicketType} the type of the tickets in the request
+   */
   getTicketType() {
     return this.#type;
   }
 
-  #Type = ['ADULT', 'CHILD', 'INFANT'];
+  #Type = ["ADULT", "CHILD", "INFANT"];
 }
